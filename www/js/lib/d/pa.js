d893865ead7d1176c07c3f469eda5387621c564f
ibrahem <ibrahem.al-radaei@outlook.com>
2024-12-19 23:30:14 +0300

    Update pa.js

--- www/js/lib/d/pa.js
+++ www/js/lib/d/pa.js
@@ -1,31 +1,5 @@
 'use strict';
 
-function get_text_api_wmcloud(title, callback) {
-	var url = 'https://medwiki.toolforge.org/get_html/rest_v1_page.php?wmcloud=1&title=' + title
-
-	const options = {
-		method: 'GET',
-		dataType: 'json',
-	};
-	return fetch(url, options)
-		.then(response => response.json())
-		.then(data => data.text)
-		.then(result => callback(result))
-}
-
-function get_text_api_new(title, callback) {
-	var url = 'https://medwiki.toolforge.org/get_html/rest_v1_page.php?title=' + title
-
-	const options = {
-		method: 'GET',
-		dataType: 'json',
-	};
-	return fetch(url, options)
-		.then(response => response.json())
-		.then(data => data.text)
-		.then(result => callback(result))
-}
-
 async function get_d_text(url) {
 	let text = "";
 
@@ -67,7 +41,5 @@ async function get_text_new(title, domain, callback) {
 }
 
 module.exports = {
-	get_text_api_new,
 	get_text_new,
-	get_text_api_wmcloud
 };
