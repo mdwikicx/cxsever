ad0a7fca24719547fc82d5e0fa4810352d4f9e09
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2026-09-04 07:24:46 +0300

    .

---
+++ www/js_live/segments_main.js
@@ -0,0 +1,75 @@
+'use strict';
+
+import Parser from './lib/lineardoc/Parser';
+import MwContextualizer from './lib/lineardoc/MwContextualizer';
+import CXSegmenter from './lib/CXSegmenter';
+
+const removableSections = {
+    "classes": [
+        "ambox",
+        "hatnote",
+        "metadata",
+        "navbar",
+        "navbox",
+        "sisterproject",
+        "sistersitebox",
+        "vertical-navbox"
+    ],
+    "rdfa": [
+        "mw:Extension/indicator",
+        "mw:Extension/templatestyles"
+    ],
+    "templates": [
+        "/^pp(-.*)?$/",
+        "/^Articles (for|with|needing|containing).*$/",
+        "/^Engvar[AB]$/",
+        "/^Use[\\sa-z]+(English|spelling|referencing)$/",
+        "/^Use [DMY]+ dates$/",
+        "/^Wikipedia articles (for|with|needing|containing).*$/",
+        "Anchor",
+        "DEFAULTSORT",
+        "Esborrany",
+        "Featured article",
+        "FR",
+        "Mdwiki revid",
+        "mdwiki revid",
+        "/^[Mm]dwiki[ _]revid$",
+        "good article",
+        "/^(.*-)?Stub$/",
+        "Short description",
+        "Void",
+        "Italic title",
+        "About",
+        "See also",
+        "Redirect",
+        "Distinguish",
+        "TOC limit",
+        "/^#unlinkedwikibase:id=Q\\d+$/",
+        "Other uses",
+        "redirect"
+    ]
+};
+
+function HtmltoSegments(source_HTML) {
+
+    const parser = new Parser(new MwContextualizer(
+        { removableSections: removableSections }
+    ), {
+        wrapSections: true
+    });
+
+    parser.init();
+    parser.write(source_HTML);
+    let parsedDoc = parser.builder.doc;
+    parsedDoc = parsedDoc.wrapSections();
+
+    const segmentedDoc = new CXSegmenter().segment(parsedDoc, "en");
+
+    const result = segmentedDoc.getHtml();
+
+    return result;
+}
+
+module.exports = {
+    HtmltoSegments: HtmltoSegments
+};
