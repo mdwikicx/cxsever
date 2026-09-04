d3f88351cd1df37b27e2e3556ede7916d1e53969
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2026-09-04 08:05:45 +0300

    .

--- www/js/run2.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d I:\medwiki\new\cxsever-repo\www\js
-nodemon ./server.js --trace-warnings
-pause
---
+++ www/run2.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d I:/MD_TOOLS/Mdwiki_Cx_Server/cxsever-repo/www
+nodemon ./js/server.js --trace-warnings
+pause
